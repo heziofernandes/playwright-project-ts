d535148cf8f9edcd0339f762f1cf6f9b1a0371d0
Hezio Fernandes <fernandes.bcc@gmail.com>
2025-10-24 22:12:24 +0100

    Fix steps to generate PDF report after Playwright tests

--- generate-pdf.ts
+++ generate-pdf.ts
@@ -6,13 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 (async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-
+  // Seu código aqui...
   const relativePath = 'playwright-report/index.html';
 
   const absolutePath = path.resolve(__dirname, relativePath);
   const fileUrl = 'file://' + absolutePath.replace(/\\/g, '/');
+
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
   await page.goto(fileUrl, { waitUntil: 'networkidle' });
   await page.pdf({ path: 'report.pdf', format: 'A4' });
